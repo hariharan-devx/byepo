235a40df8a9e4321c0e7b987979ad51e70450d0e
Hariharan <hariharan@radiantcashservices.com>
2026-02-15 15:37:07 +0530

    fix: implemented frontend authentication logic based on role and prepared readme file

--- server/src/middlewares/roleMiddleware.js
+++ server/src/middlewares/roleMiddleware.js
@@ -3,7 +3,7 @@ import CustomError from "../utils/CustomError.js";
 export const roleMiddleware = (...role) => {
   return (req, res, next) => {
     if (!role.includes(req.user.role)) {
-      return next(new CustomError(401, "You do not have permission to perform this action"));
+      return next(new CustomError(403, "You do not have permission to perform this action"));
     }
     next();
   };
